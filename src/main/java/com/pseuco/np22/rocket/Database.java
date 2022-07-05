bedd8b542dbeb75af24c80e276495b188dad1985
Mohamad <s8moalta@stud.uni-saarland.de>
2022-07-05 20:29:18 +0200

    Data Base

--- src/main/java/com/pseuco/np22/rocket/Database.java
+++ src/main/java/com/pseuco/np22/rocket/Database.java
@@ -3,6 +3,10 @@ package com.pseuco.np22.rocket;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Implementation of the central database for tickets.
  */
@@ -11,6 +15,10 @@ public class Database {
      * The tickets that are currently in the database.
      */
     private final List<Ticket> unallocated = new ArrayList<>();
+
+    private Lock ticketLock; // lock for safe access/leave to the tickets
+    private Condition isThereAcsesstoTicket; // Cheak if there is someone cheacking or changing tickets
+    private int numAvailable; // the number of tickets available
 
     /**
      * Constructs a new {@link Database}.
@@ -22,6 +30,9 @@ public class Database {
         for (var id = 0; id < coordinator.getConfig().getNumTickets(); id++) {
             this.unallocated.add(new Ticket(id));
         }
+        this.ticketLock = new ReentrantLock();
+        this.isThereAcsesstoTicket = ticketLock.newCondition();
+        this.numAvailable = coordinator.getConfig().getNumTickets();
     }
 
     /**
@@ -30,9 +41,7 @@ public class Database {
      * @return The number of tickets available in the database.
      */
     public int getNumAvailable() {
-
-        
-        throw new RuntimeException("Not implemented!");
+        return numAvailable;
     }
 
     /**
@@ -48,7 +57,30 @@ public class Database {
      * @return A list of allocated tickets.
      */
     public List<Ticket> allocate(final int numTickets) {
-        throw new RuntimeException("Not implemented!");
+        List<Ticket> AllocatedTickets = new ArrayList<>();
+        Ticket ticket = null;
+        // if there is no tickets in Data Base
+        if (numAvailable == 0) {
+            return new ArrayList<>();
+            // if there are tickets in Data Base as I asked
+        } else if (numTickets <= numAvailable) {
+            for (int i = 0; i < numTickets; i++) {
+                ticket = unallocated.get(0);
+                unallocated.remove(0);
+                numAvailable--;
+                AllocatedTickets.add(ticket);
+            }
+            return AllocatedTickets;
+            // if there are tickets but not as I asked
+        } else {
+            for (int i = 0; i < numAvailable; i++) {
+                ticket = unallocated.get(0);
+                unallocated.remove(0);
+                numAvailable--;
+                AllocatedTickets.add(ticket);
+            }
+            return AllocatedTickets;
+        }
     }
 
     /**
@@ -57,7 +89,31 @@ public class Database {
      * @param tickets The tickets to return to the database.
      */
     public void deallocate(final Iterable<Ticket> tickets) {
-        throw new RuntimeException("Not implemented!");
+        // add all tickets in unallocated List
+        tickets.forEach((ticket) -> {
+            unallocated.add(ticket);
+            numAvailable++;
+        });
     }
-    //test once again
+
+    /**
+     * get condition
+     */
+    public Condition getIsFreeToTicket() {
+        return isThereAcsesstoTicket;
+    }
+
+    /**
+     * lock for safe access/leave to the tickets
+     */
+    public void TicketLock() {
+        ticketLock.lock();
+    }
+
+    /**
+     * unlock for safe access/leave to the tickets
+     */
+    public void TicketUnlock() {
+        ticketLock.unlock();
+    }
 }
