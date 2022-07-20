5eb64295416cac65e28f1c42e07a767930945931
Mahmoud Altaha <s8maalta@stud.uni-saarland.de>
2022-07-20 21:21:22 +0200

    try without locks in balancer

--- src/main/java/com/pseuco/np22/rocket/Balancer.java
+++ src/main/java/com/pseuco/np22/rocket/Balancer.java
@@ -46,140 +46,124 @@ public class Balancer implements RequestHandler {
 
     @Override
     public void handle(Request request) {
-        balancerLock.lock();
-        try {
-            /*
-             * Implementation of the load balancer.
-             * 
-             * Hint: You must handle the `NUM_SERVERS` and `GET_SERVERS` requests here. All
-             * other requests must be redirected to individual servers (expect the `DEBUG`
-             * request which can be handled however you like).
-             * 
-             * For the `NUM_SERVERS` request you have to handle both `GET` and `POST` requests.
-             */
-            switch (request.getKind()) {
-                case NUM_SERVERS: {
-                    switch (request.getMethod()) {
-                        case GET: {
-                            /*
-                             * Query the coordinator for the number of active
-                             * (non-terminating) servers and send the number back to
-                             * the client using `respondWithInt`.
-                             */
-                            int numberofActiveServers = this.coordinator.getNumOfServers();
-                            request.respondWithInt(numberofActiveServers);
-                            break;
-                        }
-                        case POST: {
-                            /*
-                             * Obtain the new number of servers from the request using
-                             * `readInt`, scale to the given number of servers, and finally
-                             * respond with the new number of servers.
-                             */
-
-                            final var numServers = request.readInt();
-                            int IntnumberOFServer = numServers.get();
-
-                            Boolean numberIsHere = false;
-                            if (IntnumberOFServer > 0) {
-                                numberIsHere = true;
-                            }
-                            if (numberIsHere) {
-                                int newNumber = this.coordinator.scale(IntnumberOFServer);
-                                request.respondWithInt(newNumber);
-                                System.out.println("Done" + newNumber);
-                                // In this case there is no need to scale of the number of servers
-                            } else {
-                                request.respondWithInt(this.coordinator.getNumOfServers());
-                            }
-                            break;
-                        }
+        // balancerLock.lock();
+        // try {
+        /*
+         * Implementation of the load balancer.
+         * 
+         * Hint: You must handle the `NUM_SERVERS` and `GET_SERVERS` requests here. All
+         * other requests must be redirected to individual servers (expect the `DEBUG`
+         * request which can be handled however you like).
+         * 
+         * For the `NUM_SERVERS` request you have to handle both `GET` and `POST` requests.
+         */
+        switch (request.getKind()) {
+            case NUM_SERVERS: {
+                switch (request.getMethod()) {
+                    case GET: {
+                        /*
+                         * Query the coordinator for the number of active
+                         * (non-terminating) servers and send the number back to
+                         * the client using `respondWithInt`.
+                         */
+                        int numberofActiveServers = this.coordinator.getNumOfServers();
+                        request.respondWithInt(numberofActiveServers);
+                        break;
                     }
-                    break;
-                }
-                case GET_SERVERS: {
-                    /**
-                     * Query the coordinator for the active (non-terminating) servers
-                     * and send the ids of these servers to the client.
-                     * 
-                     * Hint: Use `respondWithServerIds`.
-                     */
-                    Iterable<ServerId> ids;
-                    ids = this.coordinator.getActiveServerIds();
-                    request.respondWithServerIds(ids);
-                    break;
-                }
+                    case POST: {
+                        /*
+                         * Obtain the new number of servers from the request using
+                         * `readInt`, scale to the given number of servers, and finally
+                         * respond with the new number of servers.
+                         */
 
-                case DEBUG: {
-                    /**
-                     * You are free to handle this request however you like, e.g., by sending
-                     * some useful debugging information to the client.
-                     */
-                    request.respondWithString("Happy Debugging! 🚫🐛");
-                    break;
-                }
+                        final var numServers = request.readInt();
+                        int IntnumberOFServer = numServers.get();
 
-                default:
-                    /**
-                     * The remaining requests must be handed over to a server.
-                     * 
-                     * Hint: Your implementation must be able to handle cases where a server
-                     * already terminated or is in the process of terminating when a request
-                     * for that server hits the load balancer. In case the server has already
-                     * terminated, the balancer should assign a new server to the client.
-                     * 
-                     * You must use the coordinator to obtain the mailbox of the server which
-                     * should handle the request. You must then use the mailbox of this server
-                     * to deliver the request as a message with low priority. Using our
-                     * skeleton this means constructing and sending a `MsgProcessRequest`
-                     * message to the server.
-                     */
-
-                    // check if the request of client is worked from known Server
-                    if (!request.getServerId().isEmpty()) {
-                        // check if this server is now aktive or terminated
-                        ServerId ID_associatedServerKnown = request.getServerId().get();
-
-                        // TODO: Add by Mohamad, you to see editors in calsses Server and coordinator, I gave makr
-                        // with "TODO"
-                        boolean isServerStillActive = this.coordinator.getActiveServerIds()
-                                .contains(ID_associatedServerKnown);
-                        boolean isServerInProcesOfTermination = this.coordinator.getinTerminationServersIDs()
-                                .contains(ID_associatedServerKnown);
-                        if (isServerStillActive || isServerInProcesOfTermination) {
-                            // constructing MsgProcessRequest with request
-                            Command<Server> message = new MsgProcessRequest(request);
-                            System.out.println("I BALANCER I send the Request " + request.getKind() + "  ");
-                            var mailBoxOfassociatedServerKnown = this.coordinator
-                                    .getServerMailbox(ID_associatedServerKnown);
-                            mailBoxOfassociatedServerKnown.sendLowPriority(message);
+                        Boolean numberIsHere = false;
+                        if (IntnumberOFServer > 0) {
+                            numberIsHere = true;
                         }
-                        // if (!request.getKind().equals(Kind.RESERVE_TICKET)
-                        // && (!this.coordinator.getTerminatedServerIds()
-                        // .contains(ID_associatedServerKnown))) {
-                        // // constructing MsgProcessRequest with request
-                        // Command<Server> message = new MsgProcessRequest(request);
-                        // var mailBoxOfassociatedServerKnown = this.coordinator
-                        // .getServerMailbox(ID_associatedServerKnown);
-                        // mailBoxOfassociatedServerKnown.sendLowPriority(message);
-                        // }
-                        else {
-                            // get random server from the list of active servers
-                            ServerId associatedServerID = this.coordinator.pickRandomServer();
-                            // correlate a customar with specific server
-                            request.setServerId(associatedServerID);
-                            // constructing MsgProcessRequest with request
-                            Command<Server> message = new MsgProcessRequest(request);
-                            System.out.println("I BALANCER I send the Request " + request.getKind() + "  ");
-                            // get the mail box of this picked server
-                            var mailBoxOfPickedServer = this.coordinator.getServerMailbox(associatedServerID);
-                            // send this message with low priority
-                            mailBoxOfPickedServer.sendLowPriority(message);
+                        if (numberIsHere) {
+                            int newNumber = this.coordinator.scale(IntnumberOFServer);
+                            request.respondWithInt(newNumber);
+                            System.out.println("Done" + newNumber);
+                            // In this case there is no need to scale of the number of servers
+                        } else {
+                            request.respondWithInt(this.coordinator.getNumOfServers());
                         }
-                        // if the rquest of client is new or the previous server is terminated, so then obtain a
-                        // random active server to handle this request
-                        // generally about isPresent etc.
-                    } else {
+                        break;
+                    }
+                }
+                break;
+            }
+            case GET_SERVERS: {
+                /**
+                 * Query the coordinator for the active (non-terminating) servers
+                 * and send the ids of these servers to the client.
+                 * 
+                 * Hint: Use `respondWithServerIds`.
+                 */
+                Iterable<ServerId> ids;
+                ids = this.coordinator.getActiveServerIds();
+                request.respondWithServerIds(ids);
+                break;
+            }
+
+            case DEBUG: {
+                /**
+                 * You are free to handle this request however you like, e.g., by sending
+                 * some useful debugging information to the client.
+                 */
+                request.respondWithString("Happy Debugging! 🚫🐛");
+                break;
+            }
+
+            default:
+                /**
+                 * The remaining requests must be handed over to a server.
+                 * 
+                 * Hint: Your implementation must be able to handle cases where a server
+                 * already terminated or is in the process of terminating when a request
+                 * for that server hits the load balancer. In case the server has already
+                 * terminated, the balancer should assign a new server to the client.
+                 * 
+                 * You must use the coordinator to obtain the mailbox of the server which
+                 * should handle the request. You must then use the mailbox of this server
+                 * to deliver the request as a message with low priority. Using our
+                 * skeleton this means constructing and sending a `MsgProcessRequest`
+                 * message to the server.
+                 */
+
+                // check if the request of client is worked from known Server
+                if (!request.getServerId().isEmpty()) {
+                    // check if this server is now aktive or terminated
+                    ServerId ID_associatedServerKnown = request.getServerId().get();
+
+                    // TODO: Add by Mohamad, you to see editors in calsses Server and coordinator, I gave makr
+                    // with "TODO"
+                    boolean isServerStillActive = this.coordinator.getActiveServerIds()
+                            .contains(ID_associatedServerKnown);
+                    boolean isServerInProcesOfTermination = this.coordinator.getinTerminationServersIDs()
+                            .contains(ID_associatedServerKnown);
+                    if (isServerStillActive || isServerInProcesOfTermination) {
+                        // constructing MsgProcessRequest with request
+                        Command<Server> message = new MsgProcessRequest(request);
+                        System.out.println("I BALANCER I send the Request " + request.getKind() + "  ");
+                        var mailBoxOfassociatedServerKnown = this.coordinator
+                                .getServerMailbox(ID_associatedServerKnown);
+                        mailBoxOfassociatedServerKnown.sendLowPriority(message);
+                    }
+                    // if (!request.getKind().equals(Kind.RESERVE_TICKET)
+                    // && (!this.coordinator.getTerminatedServerIds()
+                    // .contains(ID_associatedServerKnown))) {
+                    // // constructing MsgProcessRequest with request
+                    // Command<Server> message = new MsgProcessRequest(request);
+                    // var mailBoxOfassociatedServerKnown = this.coordinator
+                    // .getServerMailbox(ID_associatedServerKnown);
+                    // mailBoxOfassociatedServerKnown.sendLowPriority(message);
+                    // }
+                    else {
                         // get random server from the list of active servers
                         ServerId associatedServerID = this.coordinator.pickRandomServer();
                         // correlate a customar with specific server
@@ -192,12 +176,28 @@ public class Balancer implements RequestHandler {
                         // send this message with low priority
                         mailBoxOfPickedServer.sendLowPriority(message);
                     }
-                    break;
-            }
-
-        } finally {
-            balancerLock.unlock();
+                    // if the rquest of client is new or the previous server is terminated, so then obtain a
+                    // random active server to handle this request
+                    // generally about isPresent etc.
+                } else {
+                    // get random server from the list of active servers
+                    ServerId associatedServerID = this.coordinator.pickRandomServer();
+                    // correlate a customar with specific server
+                    request.setServerId(associatedServerID);
+                    // constructing MsgProcessRequest with request
+                    Command<Server> message = new MsgProcessRequest(request);
+                    System.out.println("I BALANCER I send the Request " + request.getKind() + "  ");
+                    // get the mail box of this picked server
+                    var mailBoxOfPickedServer = this.coordinator.getServerMailbox(associatedServerID);
+                    // send this message with low priority
+                    mailBoxOfPickedServer.sendLowPriority(message);
+                }
+                break;
         }
+
+        // } finally {
+        // balancerLock.unlock();
+        // }
 
     }
 
