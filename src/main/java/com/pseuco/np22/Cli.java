4184dd25e4dda13cd9dd3ec5a6d50479c5075017
Mahmoud Altaha <s8maalta@stud.uni-saarland.de>
2022-07-20 04:02:57 +0200

    number ticket 15

--- src/main/java/com/pseuco/np22/Cli.java
+++ src/main/java/com/pseuco/np22/Cli.java
@@ -36,7 +36,7 @@ public class Cli {
      * The amount of tickets initially available.
      */
     @Parameter(names = "-tickets")
-    private int tickets = 1000;
+    private int tickets = 15;
 
     /**
      * The timeout after which reservations expire in seconds.
